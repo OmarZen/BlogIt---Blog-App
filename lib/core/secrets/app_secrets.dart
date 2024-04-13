23a237529d0388ceb9958daba899e10c46bcdd02
OmarZen <omarwaleedzenhom2002@gmail.com>
2024-04-13 20:11:55 +0200

    BlogIt - Blog App V1

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://hdzppkirbtjccvxgnvqd.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkenBwa2lyYnRqY2N2eGdudnFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NDExMzMsImV4cCI6MjAyODQxNzEzM30.pniUgHmrE-ikzP3RNtxIKLrWAoe3B4fCunkF0Q-i_hU';
+}
